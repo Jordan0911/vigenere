b2e994799192f95b8ae75a7838ba6fd1ba3a6d91
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2026-02-11 13:38:04 +0100

    Refactor main logic and space jumper function

--- Program.cs
+++ Program.cs
@@ -1,49 +1,71 @@
 ﻿namespace vigenere
 {
     internal class Program
+{
+    static int Space_jumper(char[] alphabet, char[] word,int location)
     {
-        static void Main(string[] args)
-        {
-            bool found=false;   
-            int t = 0,q=0,s=0,a=0;
-            string word = "", worm = "";
-            char[] alphabet= "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-            Console.WriteLine("insert the word");
-            word=Console.ReadLine();
-            Console.WriteLine("insert the worm");
-            worm= Console.ReadLine();
-            char[] worm_broken = worm.ToCharArray();
-            char[] word_broken = word.ToCharArray();
-            char[] wormed_word = new char[word_broken.Length];
-            char[] coded_word= new char[word_broken.Length];    
-            for (int i = 0; i < word.Length; i++) 
-            {
-                    if (t == worm_broken.Length)
-                    {
-                        t = 0;
-                    }
-                wormed_word[i] = worm_broken[t];
-                t++;            
-            }
-            for(int i = 0;i < word_broken.Length; i++)
-            {
-                found = false;
-                while (found == false)
-                {
-                    if (s == 26)
-                    {
-                        s = 0;
-                    }
-                    if (wormed_word[q] == alphabet[s])
-                    {
-                        a = s + 1;
-                       
-                        found = true;
-                    }
-                    q++;
-                }
-            }
 
+        bool found=false;
+        int s = 0,t=0;
+        
+        found = false;
+        while (found == false)
+        {
+            if (s == 26)
+            {
+                s = 0;
+            }
+            if (word[location] == alphabet[s])
+            {
+                found = true;
+            }
+            s++;
+            t++;
         }
+        return t;
+    }
+    static void Main(string[] args)
+    {
+        int t = 0,spaces_to_jump=0,letter_location=0,q=0;
+        string word = "", worm = "",code="";
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        Console.WriteLine("insert the word");
+        word = Console.ReadLine();
+        Console.WriteLine("insert the worm");
+        worm = Console.ReadLine();
+        char[] worm_broken = worm.ToCharArray();
+        char[] word_broken = word.ToCharArray();
+        char[] wormed_word = new char[word_broken.Length];
+        char[] coded_word = word_broken;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (t == worm_broken.Length)
+                {
+                    t = 0;
+                }
+                wormed_word[i] = worm_broken[t];
+                t++;
+            }
+        for (int i = 0; i < word_broken.Length; i++)
+        {
+            if (i > wormed_word.Length)
+            {
+                q = 0;
+            }
+            letter_location = Space_jumper(alphabet, coded_word, i);
+            spaces_to_jump = letter_location + Space_jumper(alphabet, wormed_word, q);
+            if (letter_location + spaces_to_jump > 26)
+            {
+                spaces_to_jump -= 26;
+            }
+            else
+            {
+                coded_word[i] = alphabet[letter_location + spaces_to_jump];
+            }
+            q++;
+        }
+        code = string.Join("",coded_word);
+        Console.WriteLine(code);
     }
 }
